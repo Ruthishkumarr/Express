3cd870607d043afd1df222e67fe151ded1029086
Ruthishkumarr <ruthishkumarrajendran@gmail.com>
2024-03-21 08:49:26 +0530

    Changed

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require("express");
+// const express = require("express");
+import express from 'express'
 const app = express();
-const port=8000
+const port = 8000;
 
 app.get("/", (req, res) => {
-  res.status(200).send(`<h1>welcome</h1>`);
+  res.status(200).send(`<h1>welcomes</h1>`);
 });
 
 app.get("/user", (req, res) => {
-  res.status(200).send(`<h1>welcome-users</h1>`);
+  res.status(200).send(`<h1>welcome-R</h1>`);
 });
 
-app.listen(port,()=>console.log(`app is listening port ${port}`));
+app.listen(port, () => console.log(`app is listening port ${port}`));
