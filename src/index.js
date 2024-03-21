4cde2691cfa49d73b734fe0fdacca36efe278c94
Ruthishkumarr <ruthishkumarrajendran@gmail.com>
2024-03-21 11:36:41 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 // const express = require("express");
-import express from 'express'
+import express from "express";
 const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.status(200).send(`<h1>welcomes</h1>`);
-});
-
-app.get("/user", (req, res) => {
-  res.status(200).send(`<h1>welcome-R</h1>`);
+  res.status(200).send(`<h1>Welcome To Express JS</h1>`);
 });
 
 app.listen(port, () => console.log(`app is listening port ${port}`));
